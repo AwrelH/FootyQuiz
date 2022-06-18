e528cae8aa0451326b1da7b27b197dad4f038709
AwrelH <hiwa.awrel@gmail.com>
2022-06-18 20:28:45 +0000

    initial js structure

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener('DOMContentLoaded', function()) {
+    
+}
+
+
+function questionImages(){
+
+}
+
+function question(){
+
+}
+
+function questionImages(){
+
+}
+
+function questionImages(){
+
+}
+
+function keepScore(){
+
+}
+
+function highScore(){
+    
+}
