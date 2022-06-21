a738572ee0e2c643d7f462300a506ceeab8caf8f
AwrelH <hiwa.awrel@gmail.com>
2022-06-21 09:32:29 +0000

    prompt message for player to add name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,24 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-document.addEventListener('DOMContentLoaded', function()) {
-    
-}
+document.addEventListener('DOMContentLoaded', function() {
+   prompt('Please Type your player name?');
+                 
 
-
-function questionImages(){
-
-}
+});
 
 function question(){
 
-}
 
+}
 function questionImages(){
 
 }
 
-function questionImages(){
-
-}
 
 function keepScore(){
 
 }
 
 function highScore(){
-    
+
 }
