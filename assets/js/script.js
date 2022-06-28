395ae9c45b7c57f3c8a665119009d69c0dbff550
AwrelH <hiwa.awrel@gmail.com>
2022-06-28 11:46:06 +0000

    debug why images in button don't appear, change back to innerHTML istead of innerText

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function () {
-  
 
-  
+
+
 });
 //APPEND div, h1, p of the start page when entering page
 let myIntroDiv = document.createElement("div");
@@ -32,181 +32,172 @@ myParagraph.append(myParagraphText, myIntroButton);
 let myDiv = document.getElementById("myDiv");
 myDiv.append(myHeading, myParagraph);
 
-//
+//Adding eventlistener to introbutton
 let introButton = document.getElementById("introButton");
 introButton.addEventListener("click", welcome);
 
 
+let i = 0;
+let right = 0;
+let wrong = 0;
 
+function startGame(question) {
+  let option1 = question.answers.a;
+  let option2 = question.answers.b;
+  let option3 = question.answers.c;
+  let option4 = question.answers.d;
+  let quest = question.question;
+  document.getElementById("question").innerHTML = quest;
+  document.getElementById("optionA").innerHTML = option1;
+  document.getElementById("optionB").innerHTML = option2;
+  document.getElementById("optionC").innerHTML = option3;
+  document.getElementById("optionD").innerHTML = option4;
 
+}
 
-
-
-
-
-  let i = 0;
-  let right = 0;
-  let wrong = 0;
-  function startGame(question) {
-    let option1 = question.answers.a;
-    let option2 = question.answers.b;
-    let option3 = question.answers.c;
-    let option4 = question.answers.d;
-    let quest = question.question;
-    document.getElementById("question").innerText = quest;
-    document.getElementById("optionA").innerText = option1;
-    document.getElementById("optionB").innerText = option2;
-    document.getElementById("optionC").innerText = option3;
-    document.getElementById("optionD").innerText = option4;
-   
+function answered(option) {
+  correct = questions[i].correctAnswer;
+  if (correct === option) {
+    console.log("thats right");
+    right++;
+    document.getElementById("rightAnswers").innerText = right.toString();
+  } else {
+    wrong++;
+    document.getElementById("wrongAnswers").innerText = wrong.toString();
   }
-  function answered(option) {
-    correct = questions[i].correctAnswer;
-    if (correct === option) {
-      console.log("thats right");
-      right++;
-      document.getElementById("rightAnswers").innerText = right.toString();
-    } else {
-      wrong++;
-      document.getElementById("wrongAnswers").innerText = wrong.toString();
-    }
-    i++;
-    // if not the end of the game onto the next question
-    if (i < 10) {
-      startGame(questions[i]);
-    } else {
-      console.log("end of game");
-      // call a function that gives end of game message
-      endGame();
-    }
+  i++;
+  // if not the end of the game onto the next question
+  if (i < 10) {
+    startGame(questions[i]);
+  } else {
+    console.log("end of game");
+    // call a function that gives end of game message
+    endGame();
   }
-  function endGame() {
-    document.getElementById("answerContainer").style.display = "none";
-    //removes the absolute position from the h2 element and keeps score boxes below and not under it
-    //when removing display 
-    document.getElementById('question').style.position = "initial";
-    document.getElementById(
-      "question"
-    ).innerHTML = `You have reached the end of the game and you got ${right} correct answer(s)`;
+}
 
-  }
-  
-  /**
-   *
-   * All the questions and answers within an array, seperated within objects
-   */
-  const questions = [
-    {
-      question: "Who are the world champion of WC 2018?",
-      answers: {
-        a: "Croatia <img alt='flag of croatia' src='assets/images/q-images/croatia.png'>",
+function endGame() {
+  document.getElementById("answerContainer").style.display = "none";
+  //removes the absolute position from the h2 element and keeps score boxes below and not under it
+  //when removing display 
+  document.getElementById('question').style.position = "initial";
+  document.getElementById("question").innerText =
+    `You have reached the end of the game and you got ${right} correct answer(s)`;
+
+}
+
+/**
+ *
+ * All the questions and answers within an array, seperated within objects
+ */
+const questions = [{
+    question: "Who are the world champion of WC 2018?",
+    answers: {
+      a: "Croatia <img alt='flag of croatia' src='assets/images/q-images/croatia.png'>",
       b: "Spain <img alt='flag of spain' src='assets/images/q-images/spain.png'>",
       c: "Brazil <img alt='flag of brazil' src='assets/images/q-images/brazil.png'>",
       d: "France <img alt='flag of france' src='assets/images/q-images/france.png'>",
-      },
-      correctAnswer: "d",
     },
-    {
-      question: "Which world cups did Zlatan Ibrahimovic play?",
-      answers: {
-        a: "1998 and 2002",
-        b: "2002 and 2006",
-        c: "He didn't",
-        d: " 2006 and 2010",
-      },
-      correctAnswer: "b",
+    correctAnswer: "d",
+  },
+  {
+    question: "Which world cups did Zlatan Ibrahimovic play?",
+    answers: {
+      a: "1998 and 2002",
+      b: "2002 and 2006",
+      c: "He didn't",
+      d: " 2006 and 2010",
     },
-    {
-      question: "Who missed the  last penalty in wc 1994?",
-      answers: {
-        a: "Romario <img alt='romario' src='assets/images/q-images/romario.png'>",
+    correctAnswer: "b",
+  },
+  {
+    question: "Who missed the  last penalty in wc 1994?",
+    answers: {
+      a: "Romario <img alt='romario' src='assets/images/q-images/romario.png'>",
       b: "Costacurta <img src='assets/images/q-images/costacurta.png'>",
       c: "Robert Baggio <img src='assets/images/q-images/baggio.png'>",
       d: "Ravanelli <img src='assets/images/q-images/ravanelli.png'>",
-      },
-      correctAnswer: "c",
     },
-    {
-      question: "What team came third in the world cup 2002?",
-      answers: {
-        a: "Turkey <img alt='flag of turkey' src='assets/images/q-images/turkey.png'>",
+    correctAnswer: "c",
+  },
+  {
+    question: "What team came third in the world cup 2002?",
+    answers: {
+      a: "Turkey <img alt='flag of turkey' src='assets/images/q-images/turkey.png'>",
       b: "South Korea <img alt='flag of south korea' src='assets/images/q-images/southKorea.png'>",
       c: "Mexico <img alt='flag of mexico' src='assets/images/q-images/mexico.png'>",
       d: "Spain <img alt='flag of spain' src='assets/images/q-images/spain.png'>",
-      },
-      correctAnswer: "a",
     },
-    {
-      question:
-        "The name of the instrument that was popular in the WC 2008?",
-      answers: {
-        a: "Saxophone",
+    correctAnswer: "a",
+  },
+  {
+    question: "The name of the instrument that was popular in the WC 2008?",
+    answers: {
+      a: "Saxophone",
       b: "Vuvuzela",
       c: "Guitar",
       d: "flute",
-      },
-      correctAnswer: "b",
     },
-    {
-      question: "When did Italy play the world cup last?",
-      answers: {
-        a: "2018",
-        b: "1994",
-        c: "2002",
-        d: "2014",
-      },
-      correctAnswer: "d",
+    correctAnswer: "b",
+  },
+  {
+    question: "When did Italy play the world cup last?",
+    answers: {
+      a: "2018",
+      b: "1994",
+      c: "2002",
+      d: "2014",
     },
-    {
-      question: "How many world cup goals have Cristiano Ronaldo scored?",
-      answers: {
-        a: "7 goals",
-        b: "0 goals",
-        c: "6 goals",
-        d: "9 goals",
-      },
-      correctAnswer: "a",
+    correctAnswer: "d",
+  },
+  {
+    question: "How many world cup goals have Cristiano Ronaldo scored?",
+    answers: {
+      a: "7 goals",
+      b: "0 goals",
+      c: "6 goals",
+      d: "9 goals",
     },
-    {
-      question: "Where will the world cup be played 2022?",
-      answers: {
-        a: "England <img alt='flag of england' src='assets/images/q-images/england.png'>",
-        b: "France <img alt='flag of france' src='assets/images/q-images/france.png'>",
-        c: "Qatar <img alt='flag of qatar' src='assets/images/q-images/qatar.png'>",
-        d: "Australia <img alt='flag of australia' src='assets/images/q-images/australia.png'>",
-      },
-      correctAnswer: "c",
+    correctAnswer: "a",
+  },
+  {
+    question: "Where will the world cup be played 2022?",
+    answers: {
+      a: "England <img alt='flag of england' src='assets/images/q-images/england.png'>",
+      b: "France <img alt='flag of france' src='assets/images/q-images/france.png'>",
+      c: "Qatar <img alt='flag of qatar' src='assets/images/q-images/qatar.png'>",
+      d: "Australia <img alt='flag of australia' src='assets/images/q-images/australia.png'>",
     },
-    {
-      question: "What is the name of Maradonas famous goal against England?",
-      answers: {
-        a: "Foot of God <img alt='a foot' src='assets/images/q-images/foot.png'>",
+    correctAnswer: "c",
+  },
+  {
+    question: "What is the name of Maradonas famous goal against England?",
+    answers: {
+      a: "Foot of God <img alt='a foot' src='assets/images/q-images/foot.png'>",
       b: "Chest of God <img alt='a hairy chest' src='assets/images/q-images/chest.png'>",
       c: "Hand of God <img alt='a hand' src='assets/images/q-images/hand.png'>",
       d: "Head of God <img alt='a head' src='assets/images/q-images/head.png'>",
-      },
-      correctAnswer: "c",
     },
-    {
-      question: "What year was the first world cup?",
-      answers: {
-        a: "13 july 1930",
-        b: "8 June 1958",
-        c: "11 July 1966",
-        d: "27 May 1934",
-      },
-      correctAnswer: "a",
+    correctAnswer: "c",
+  },
+  {
+    question: "What year was the first world cup?",
+    answers: {
+      a: "13 july 1930",
+      b: "8 June 1958",
+      c: "11 July 1966",
+      d: "27 May 1934",
     },
-  ];
+    correctAnswer: "a",
+  },
+];
 
-  function welcome() {
-    document.getElementById("myDiv").style.display = "none";
-    startGame(questions[i]);
-    let playerName = window.prompt("Type your player name", "Player");
+function welcome() {
+  document.getElementById("myDiv").style.display = "none";
+  startGame(questions[i]);
+  let playerName = window.prompt("Type your player name", "Player");
   if (playerName === null) {
     playerName = "Player";
   }
   document.getElementById("playerName").innerText = playerName;
-    
-  }
-  
+}
